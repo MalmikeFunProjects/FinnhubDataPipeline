62c87c38bf286a197515a0a9658eec992a53dac3
Male Michael <malmike21@gmail.com>
2025-02-12 18:54:53 +0100

    dockerise the modules

---
+++ FinnhubProducer/resources/scripts/entryPoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Initialize Conda
+eval "$(conda shell.bash hook)"
+
+# Activate the Conda environment
+conda activate finnhubDPL
+
+# Run the Python script and capture the exit code
+python finnhub_producer.py
+EXIT_CODE=$?
+
+# Log success or failure
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "SUCCESS" > /tmp/healthcheck.log
+else
+    echo "FAILURE" > /tmp/healthcheck.log
+fi
+
+# Exit with the same exit code as the script
+# exit $EXIT_CODE
+
+# Keep container alive (useful for debugging)
+tail -f /dev/null
