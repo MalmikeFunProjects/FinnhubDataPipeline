845ea544576f51e4942f1c5cf7897719dd3b8a67
Male Michael <malmike21@gmail.com>
2025-03-12 17:18:57 +0100

    remove unused cassandra models

--- FinnhubConsumer/app/cassandra_client/models/FinnhubTrade.py
+++
@@ -1,9 +0,0 @@
-from cassandra.cqlengine.models import Model
-from cassandra.cqlengine import columns
-
-class FinnhubTrade(Model):
-    __keyspace__ = "market"
-    __table_name__ = "finnhub_trades"
-    event_timestamp = columns.BigInt(primary_key=True, partition_key=True)
-    symbol = columns.Text(primary_key=True)
-    price = columns.Double()
