cac279b3bf89d3e0351240fc22d89aec67e79e67
Male Michael <malmike21@gmail.com>
2025-03-12 18:03:19 +0100

    altered table names in model

--- FinnhubConsumer/app/cassandra_client/models/CompanySymbol.py
+++ FinnhubConsumer/app/cassandra_client/models/CompanySymbol.py
@@ -3,5 +3,5 @@ from cassandra.cqlengine import columns
 
 class CompanySymbol(Model):
     __keyspace__ = "market"
-    __table_name__ = "company_symbols"
+    __table_name__ = "company_symbol"
     symbol = columns.Text(primary_key=True)
--- FinnhubConsumer/app/cassandra_client/models/LatestPrice.py
+++ FinnhubConsumer/app/cassandra_client/models/LatestPrice.py
@@ -3,7 +3,7 @@ from cassandra.cqlengine import columns
 
 class LatestPrice(Model):
     __keyspace__ = "market"
-    __table_name__ = "latest_prices"
+    __table_name__ = "latest_price"
     event_timestamp = columns.BigInt(primary_key=True, partition_key=True)
     symbol = columns.Text(primary_key=True)
     last_price = columns.Double()
