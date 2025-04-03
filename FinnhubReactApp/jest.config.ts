f793d1ea410903c432f9e0694a26b1f5cb3439e0
Male Michael <malmike21@gmail.com>
2025-04-03 16:45:40 +0200

    add jest

---
+++ FinnhubReactApp/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+const jestConfig: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@/app/(.*)$': '<rootDir>/styles/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+  },
+};
+
+export default jestConfig;
