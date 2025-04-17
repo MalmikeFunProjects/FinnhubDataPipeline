f51f8f0f3a8aaf4b16506dc99d346e711e5abd30
Male Michael <malmike21@gmail.com>
2025-04-17 17:54:25 +0200

    add test set up for react app

--- FinnhubReactApp/jest.config.ts
+++ FinnhubReactApp/jest.config.ts
@@ -1,15 +1,28 @@
-import type { Config } from '@jest/types';
-const jestConfig: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/utils/(.*)$': '<rootDir>/utils/$1',
-    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
-    '^@/app/(.*)$': '<rootDir>/styles/$1',
-    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+// import type { Config } from "@jest/types";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const jestConfig = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$", // Add this line to include .tsx files
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest", // Explicitly handle both .ts and .tsx files
   },
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@/hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@/app/(.*)$": "<rootDir>/src/styles/$1",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "^react-chartjs-2$": "<rootDir>/__mocks__/react-chartjs-2.js"
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // moduleDirectories: ['node_modules', '<rootDir>']
 };
 
-export default jestConfig;
+export default createJestConfig(jestConfig);
---
+++ FinnhubReactApp/jest.setup.ts
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom";
+
+jest.mock('react-chartjs-2');
+jest.mock("@/components/StockSummary/StockSummaryProvider");
+jest.mock("@/utils");
